ef959a280b84e7c0b42db074a3a1f2cddb8352fd
Gabriel Barbosa <gabrielgbsspm@hotmail.com>
2022-05-17 21:04:54 -0300

    JS's comentados

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,7 +1,12 @@
+/* escreve no final do corpo do html */
 document.write("<h1>Estamos executando o script.js</h1>");
+/* escreve no final do corpo do html */
 document.write("Esse script está em um arquivo externo e é referenciado no header do HTML");
+/* escreve no console */
 console.log("Esse script está no cabeçalho do HTML");
+/* escreve no console */
 console.log("Primeiro");
+/* escreve no console */
 console.log("Segundo");
 
 // Comentário de uma linha
@@ -13,9 +18,13 @@ console.log("Segundo");
 */
 
 // Tipagem fraca
+/* cria a variavel */
 var teste = 1;
+/* escreve no final do corpo do html */
 document.write("<hr>O valor da variável teste é " + teste + "<hr>");
+/* mudar o valor da variavel */
 teste = 'joao';
+/* escreve no final do corpo do html */
 document.write("O valor da variável teste agora é " + teste + "<hr>");
 
 // Variável global - criada sem necessidade de declaração
@@ -23,11 +32,15 @@ nome = 'joao';
 console.log(nome)
 
 // Formas de declaração de variáveis
+/* cria uma variavel global */
 var   v1 = 5.25;
-let   v2 = null; 
+/* cria uma variavel com escopo */
+let   v2 = null;
+/* cria uma constante */
 const v3 = 'teste'
 
-console.log(typeof(v1));
+/* escreve no console o tipo das variaveis */
+console.log(`v1: {typeof(v1)}`);
 console.log(typeof(v2));
 console.log(typeof(v3));
 console.log(typeof NaN); // tipo number
@@ -44,16 +57,19 @@ console.log('ele disse "vtnc"');
 
 // adicionar uma lista no body do HTML
 var lista = ['Arroz', 'Feijão', 'carne','macarrão'];
+/* cria uma lista no final da pagina */
 var listaUl = document.createElement('ul');
+/* acha os elementos com a tag 'body' */
 var body = document.getElementsByTagName('body');
 
 console.log(listaUl);
 console.log(body);
 
 //alert("Vamos criar uma lista aqui!");
-
+/* adiciona a lista no corpo do html */
 body[0].appendChild(listaUl);
 
+/* cria ementos de uma lista com texto e adiciona na lista criar anteriormente */
 for(var  i=0;i<lista.length;i++) {
     var liFor = document.createElement('li');
     var textoLi = document.createTextNode(lista[i]);
@@ -63,6 +79,7 @@ for(var  i=0;i<lista.length;i++) {
 
 document.write("A lista não ordenada <b>ACIMA</b> foi criada dinamicamente via JavaScript");
 
+/* criar um objeto */
 var obj = {
     nome: "Joao",
     sobrenome: "Furtado",
--- HTML/js/script2.js
+++ HTML/js/script2.js
@@ -1,2 +1,4 @@
+/* escreve no final do corpo do html */
 document.write("<h1>Estamos executando o script2.js</h1>");
+/* escreve no final do corpo do html */
 document.write("Esse script está em um arquivo externo e é referenciado no final do body do HTML");
